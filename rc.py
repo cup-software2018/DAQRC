f32e791c269c5691d7abe879938b6d605f8e47d3
Jaison Lee <jsahnlee@gmail.com>
2026-04-29 11:52:03 +0900

    refactor: remove redundant Slot decorators from state and stats callback methods in rc.py

--- rc.py
+++ rc.py
@@ -430,7 +430,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self.RunSocket.close()
                 self.RunSocket = None
 
-    @Slot(object)
     def on_stats_received(self, stats):
         """
         Slot called by MonitorPollerThread with GET_STATS result.
@@ -447,7 +446,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.update_run_stats_display()
 
-    @Slot(int, object)
     def on_state_received(self, new_state, reply_dict):
         """
         Slot called by DAQStatePollerThread.
